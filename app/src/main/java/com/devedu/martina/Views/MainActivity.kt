48398de90ecf8237ac67b736eb34cc566562c81c
Eduardo <eduardorett@hotmail.com>
2021-11-17 17:27:42 -0300

    commit

--- app/src/main/java/com/devedu/martina/Views/MainActivity.kt
+++ app/src/main/java/com/devedu/martina/Views/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
-import com.devedu.martina.R
+import com.devedu.CadrastoDeNomes.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 
